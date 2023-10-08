dd498bb1f319e98d3e486b17299e7fe49a29a07c
Space_X <bulatgilfanov10@mail.ru>
2023-10-08 20:25:57 +0300

    test

--- main.py
+++ main.py
@@ -1,28 +1,164 @@
 import pygame
+import random
 
-win = pygame.display.set_mode((600, 400))
+pygame.init()
+win = pygame.display.set_mode((1000, 700))
 
-x = 100
-y = 50
+pygame.mixer.music.load("sound/Vandeta.mp3")
+pygame.mixer.music.play(-1)
+lose = pygame.image.load("fon/lose.jpg")
+menu = pygame.image.load("fon/menu.jpg")
+player = pygame.image.load("sprites/ship.png")
+player_size = (250,250)
+player = pygame.transform.scale(player, (player_size[0], player_size[1]))
+background = pygame.image.load("fon/space.jpg")
+button_sound = pygame.mixer.Sound("sound/click.mp3")
+laser = pygame.image.load("sprites/laserRed.png")
+enemy = pygame.image.load("sprites/meteorBrown.png")
+enemy_size = (100, 100)
+enemy = pygame.transform.scale(enemy, (enemy_size[0], enemy_size[1]))
 
-a =50
-b =10
 
-while True:
+x = 350
+y = 500
+speed = 5
 
+clock = pygame.time.Clock()
+
+
+def start_game():
+
+    run = True
+    while(run):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+class Button():
+
+    def __init__(self, width, height):
+        self.widht = width
+        self.height = height
+        self.inactive_clr = (13, 162, 58)
+        self.active_clr = (23, 204, 58)
+
+    def draw(self, x, y, message, action=None, font_size = 30):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x < mouse[0] < x + self.widht and y < mouse[1] < y + self.height:
+            pygame.draw.rect(win, self.active_clr, (x, y, self.widht, self.height))
+
+            if click[0] == 1:
+                pygame.mixer.Sound.play(button_sound)
+                pygame.time.delay(300)
+                if action is not None:
+                    action()
+        else:
+            pygame.draw.rect(win, self.inactive_clr, (x, y, self.widht, self.height))
+
+        print_text(message=message, x=x+10, y=-y+10, font_size=font_size)
+
+
+
+class Circle():
+    def __init__(self, win, color, x, y):
+        self.win = win
+        self.color = color
+        self.x = x
+        self.y = y
+        self.vel = 5
+        self.dir = 'right'
+
+    def draw(self):
+        self.win.blit(enemy, (self.x, self.y))
+
+    def horizontal_movement(self):
+        self.y += self.vel
+
+def print_text(message, x, y, font_color=(0, 0, 0), font_type='gabriola', font_size=30):
+    font_type = pygame.font.Font(font_type, font_size)
+    text = font_type.render(message,True, font_color)
+    win.blit(text, (x, y))
+
+list_circles = []
+
+start_ticks=pygame.time.get_ticks() #starter tick
+
+
+def show_menu():
+
+    start_btn = Button(300, 70)
+
+    show = True
+    while show:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        win.blit(menu, (0, 0))
+        start_btn.draw(300, 200, "Start game", start_game, 50)
+
+        pygame.display.update()
+        clock.tick(60)
+
+run = True
+while(run):
+
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            run = False
 
-    b = b + 1
-    if b > 500:
-        b = 0
 
-    win.fill((220, 255, 100))
-    pygame.draw.rect(win, (10, 25, 234), (x, y, 100, 150))
-    pygame.draw.circle(win, (0, 57, 169), (a, b), 50)
+
+
+    win.blit(background, (0, 0))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= speed
+    elif keys[pygame.K_RIGHT]:
+        x += speed
+    elif keys[pygame.K_UP]:
+        y -= speed
+    elif keys[pygame.K_DOWN]:
+        y += speed
+
+
+    win.blit(player,( x, y))
+
+    seconds = (pygame.time.get_ticks() - start_ticks) / 1000  # calculate how many seconds
+    if seconds > 2:
+        start_ticks = pygame.time.get_ticks()  # starter tick
+
+
+        color = random.choices(range(256), k=3)
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+        list_circles.append(Circle(win, color, random.randint(0, 1000), 0))
+
+
+
+    player_rect = pygame.Rect((x + 45, y), (90, 60))
+
+    for i in list_circles:
+        if player_rect.collidepoint((i.x, i.y)):
+            win.blit(lose, (0, 0))
+            run = False
+        i.draw()
+        i.horizontal_movement()
+
+
     pygame.display.update()
-
-    pygame.time.delay(10)
 
 
--- test.py
+++ test.py
@@ -11,3 +11,83 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
         self.image = pygame.transform.scale(player_img, (50, 38))
+
+
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((1000, 900))
+
+
+player = pygame.image.load("sprites/ship.png")
+background = pygame.image.load("fon/space.jpg")
+
+x = 350
+y = 500
+speed = 10
+
+clock = pygame.time.Clock()
+run = True
+while (run):
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= speed
+    elif keys[pygame.K_RIGHT]:
+        x += speed
+    elif keys[pygame.K_UP]:
+        y -= speed
+    elif keys[pygame.K_DOWN]:
+        y += speed
+
+    win.blit(background, (0, 0))
+    win.blit(player, (x, y))
+    pygame.display.update()
+
+pygame.quit()
+
+
+win, color, random.randint(0, 1000), 50
+color = random.choices(range(256), k=3)
+
+
+
+lastMove = "up"
+
+class Laser():
+    def __init__(self, x, y, radius, facing):
+        self.facing = facing
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.vel = 8 * facing
+
+    def draw(self):
+        self.win.blit(laser, (self.x, self.y), self.radius)
+
+    def drawWindow(self):
+        for bullet in bullets:
+            bullet.draw(win)
+
+        pygame.display.update()
+
+        win.blit(background, (0, 0))
+
+        for bullet in bullets:
+            if bullet.x < 500 and bullet.x > 0:
+                bullet.x += bullet.vel
+            else:
+                bullets.pop(bullets.index(bullet))
+
+                if gameplay:
+                else:
+
+                    if gameplay:
+                        mouse = pygame.mouse.get_pos()
+                    if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+                        gampelay = False
